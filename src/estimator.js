c4dfd46ee79af262e9f44517a193eaca6a9d0b34
Idowu Daramola <idowu.daramola@gmail.com>
2020-04-10 14:23:41 +0100

    changed estimator to pass test

--- src/estimator.js
+++ src/estimator.js
@@ -1,9 +1,34 @@
 const covid19ImpactEstimator = (data) => {
   const input = data;
+  const impact = { currentlyInfected: input.reportedCases * 10 };
+  const severeImpact = { currentlyInfected: input.reportedCases * 50 };
+
+
+  const { periodType } = input;
+  const { timeToElapse } = input;
+
+  if (periodType === 'months') {
+    const days = timeToElapse * 30;
+    const factor = parseInt(days / 3, 10);
+    impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
+    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
+  } else if (periodType === 'weeks') {
+    const days = timeToElapse * 7;
+    const factor = parseInt(days / 3, 10);
+    impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
+    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
+  } else {
+    const days = timeToElapse;
+    const factor = parseInt(days / 3, 10);
+    impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
+    severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
+  }
+
+
   return {
     data: input,
-    impact: {},
-    severeImpact: {}
+    impact,
+    severeImpact
   };
 };
 
