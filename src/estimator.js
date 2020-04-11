5b53f622ce57cb959a77aa81112d365123980171
Idowu Daramola <idowu.daramola@gmail.com>
2020-04-11 16:00:16 +0100

    corrections for challenge 3

--- src/estimator.js
+++ src/estimator.js
@@ -31,12 +31,12 @@ const covid19ImpactEstimator = (data) => {
   severeImpact.hospitalBedsByRequestedTime = (
     parseInt(0.35 * input.totalHospitalBeds - severeImpact.severeCasesByRequestedTime, 10)
   );
-  impact.casesForICUByRequestedTime = 0.5 * impact.infectionsByRequestedTime;
-  severeImpact.casesForICUByRequestedTime = 0.5 * severeImpact.infectionsByRequestedTime;
-  impact.casesForVentilatorsByRequestedTime = 0.2 * impact.infectionsByRequestedTime;
-  severeImpact.casesForVentilatorsByRequestedTime = 0.2 * severeImpact.infectionsByRequestedTime;
-  impact.dollarsInFlight = (impact.infectionsByRequestedTime * 0.65 * 1.5) / 30;
-  severeImpact.dollarsInFlight = (severeImpact.infectionsByRequestedTime * 0.65 * 1.5) / 30;
+  impact.casesForICUByRequestedTime = parseInt(0.5 * impact.infectionsByRequestedTime, 10);
+  severeImpact.casesForICUByRequestedTime = parseInt(0.5 * severeImpact.infectionsByRequestedTime, 10);
+  impact.casesForVentilatorsByRequestedTime = parseInt(0.2 * impact.infectionsByRequestedTime, 10);
+  severeImpact.casesForVentilatorsByRequestedTime = parseInt(0.2 * severeImpact.infectionsByRequestedTime, 10);
+  impact.dollarsInFlight = parseInt((impact.infectionsByRequestedTime * 0.65 * 1.5) / 30, 10);
+  severeImpact.dollarsInFlight = parseInt((severeImpact.infectionsByRequestedTime * 0.65 * 1.5) / 30, 10);
 
 
   return {
